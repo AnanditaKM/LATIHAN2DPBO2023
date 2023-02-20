d5cc016e4677ae1d76d1e419a59453ef309b44c9
AnanditaKM <100897554+AnanditaKM@users.noreply.github.com>
2023-02-20 17:10:32 +0700

    Update Human.java

--- Java/program/Human.java
+++ Java/program/Human.java
@@ -1,3 +1,6 @@
+/*Saya Anandita K NIM 2101114 mengerjakan soal Latihan 2 dalam mata kulia
+h Desain dan Pemrograman Berorientasi Objek untuk keberkahanNya maka saya 
+tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin. */
 class Human{
     private String NIK;
     private String nama;
